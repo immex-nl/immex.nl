6c9263855b5c28409e1a94df631e05cb47168b7b
410web <studio.410web@gmail.com>
2022-07-06 01:12:19 +0500

    frame

---
+++ site/web/app/themes/immex/home.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+
+<div id="home">
+    <header>
+        <a href="/">Markets</a>
+        <a href="/trade">Trade</a>
+    </header>
+    <iframe src="https://www.bitay.com.tr/en/markets"></iframe>
+</div>
