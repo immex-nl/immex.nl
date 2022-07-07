667768e7ae9b268511d58c1c258db55677cb56b4
410web <studio.410web@gmail.com>
2022-07-07 14:34:32 +0500

    sage init

--- site/web/app/themes/example/page-trade.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-<div id="trade">
-    <header>
-        <a href="/">Markets</a>
-        <a href="/trade">Trade</a>
-    </header>
-	<iframe src="https://www.bitay.com.tr/en/t/BTCUSDT"></iframe>
-</div>
